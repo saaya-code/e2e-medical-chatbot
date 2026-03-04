05ebcdc105e0012443d16cd1bcbfb080b6d45041
entbappy <entbappy73@gmail.com>
2026-03-04 20:46:15 +0600

    setup file updated

---
+++ app.py
@@ -0,0 +1,2 @@
+from src.helper import main
+
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages 
+
+setup(
+    name='medical_chatbot',
+    version='0.1',
+    author="Boktiar Ahmed Bappy",
+    author_email="entbappy73@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
